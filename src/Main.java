7395ddba8f5fe7359089e34b716feb94219f20e8
Secretfi1e <beyzadeemin9@gmail.com>
2024-05-01 12:26:23 +0400

    2ci sual ayri ayri yazdir

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-       // Bir metod yaradın, bu metod vasitəsilə
-       // verilən bütün hərflər ayrı-ayrılıqda çapa çıxsın.
+        // Bir metod yaradın, bu metod vasitəsilə
+        // verilən bütün hərflər ayrı-ayrılıqda çapa çıxsın.
+        String text = "a::b::c::d::e";
 
-         String text = "a b,c.d/e";
-         for(char emin : text.toCharArray()){
-        if (Character.isLetter(emin)){
-           System.out.println(emin);
-    }
-}}}
-   // verilen hersey ayri ayriliqda capa cixsin
+        for(char fayl : text.toCharArray()){
+        if(Character.isLetter(fayl)){
+            System.out.println(fayl);
+        }
+
+    }}}
+
+         //3 cu sual
+
+        // String text = "a b,c.d/e";
+      //   for(char emin : text.toCharArray()){
+       // if (Character.isLetter(emin)){
+        //   System.out.println(emin);
+  //  }
+//}}}
+
+   // 3un basqa cur yazilisi verilen hersey ayri ayriliqda capa cixsin
 
     //   String text = "a b,c.d/e";
       //  for (int i = 0; i < text.length(); i++) {
